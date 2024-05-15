c9f9d268ba826d11268e8537e7b0ef807926ef2d
BrunoSX <brunodeveloper85@gmail.com>
2024-05-15 16:32:26 -0300

    server: Disable hooks that time out

--- server/user.h
+++ server/user.h
@@ -97,7 +97,8 @@ extern void cleanup_clipboard_thread( struct thread *thread );
 
 extern void remove_thread_hooks( struct thread *thread );
 extern unsigned int get_active_hooks(void);
-extern struct thread *get_first_global_hook( int id );
+extern struct thread *get_first_global_hook( int id, thread_id_t *thread_id, client_ptr_t *proc );
+extern void disable_hung_hook( struct desktop *desktop, int id, thread_id_t thread_id, client_ptr_t proc );
 
 /* queue functions */
 
